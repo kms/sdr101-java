da5ad7e3de065a9eb611b57a8b293c2ac98779e4
Karl-Martin Skontorp <kms@skontorp.net>
2008-11-03 21:01:34 +0000

    M    src/test/java/org/picofarad/sdr101/LocalOscillatorSourceTest.java M    src/main/java/org/picofarad/sdr101/LocalOscillatorSource.java
    	*) Added LO factory, phase offset and ditto tests.
    
    
    git-svn-id: svn+ssh://ginandtonic.no/home/kms/.svnrepos/sdr101-java@750 bec0a6a6-59ba-0310-824c-86e0bd1f4ed1

--- src/main/java/org/picofarad/sdr101/LocalOscillatorSource.java
+++ src/main/java/org/picofarad/sdr101/LocalOscillatorSource.java
@@ -4,18 +4,29 @@ package org.picofarad.sdr101;
 public class LocalOscillatorSource implements SignalBlock {
     private int sampleRate;
     private double frequency;
+    private double phaseOffset;
     private int sampleNumber;
 
-    public LocalOscillatorSource(int sr, double f) {
+    public static LocalOscillatorSource factory(int sr, double f, double p) {
+	return new LocalOscillatorSource(sr, f, p);
+    }
+
+    public static LocalOscillatorSource factory(int sr, double f) {
+	return new LocalOscillatorSource(sr, f, 0.0);
+    }
+
+    private LocalOscillatorSource(int sr, double f, double p) {
 	sampleRate = sr;
 	frequency = f;
+	phaseOffset = p;
     }
 
     public double out() {
 	double r;
 	double position = ((double) sampleNumber) / ((double) sampleRate);
+	double offset = phaseOffset / 360;
 
-	r = Math.sin(2 * Math.PI * position * frequency);
+	r = Math.sin(2 * Math.PI * (position + offset) * frequency);
 
 	sampleNumber++;
 	if (sampleNumber == sampleRate) {
--- src/test/java/org/picofarad/sdr101/LocalOscillatorSourceTest.java
+++ src/test/java/org/picofarad/sdr101/LocalOscillatorSourceTest.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 public class LocalOscillatorSourceTest {
     @Test
     public void testOutAtOneFourth() {
-	LocalOscillatorSource lo = new LocalOscillatorSource(4, 1);
+	LocalOscillatorSource lo = LocalOscillatorSource.factory(4, 1);
 
 	Assert.assertEquals(0.0, lo.out(), 0.0001);
 	Assert.assertEquals(1.0, lo.out(), 0.0001);
@@ -16,7 +16,7 @@ public class LocalOscillatorSourceTest {
 
     @Test
     public void testOutAtOneEight() {
-	LocalOscillatorSource lo = new LocalOscillatorSource(8, 1);
+	LocalOscillatorSource lo = LocalOscillatorSource.factory(8, 1);
 
 	Assert.assertEquals(0.0, lo.out(), 0.001);
 	Assert.assertEquals(0.707, lo.out(), 0.001);
@@ -30,7 +30,7 @@ public class LocalOscillatorSourceTest {
 
     @Test
     public void testOutWithMultipleCycles() {
-	LocalOscillatorSource lo = new LocalOscillatorSource(4, 1);
+	LocalOscillatorSource lo = LocalOscillatorSource.factory(4, 1);
 
 	for (int i = 0; i < 1000; i++) {
 	    Assert.assertEquals(0.0, lo.out(), 0.0001);
@@ -39,4 +39,35 @@ public class LocalOscillatorSourceTest {
 	    Assert.assertEquals(-1.0, lo.out(), 0.0001);
 	}
     }
+
+    @Test
+    public void testOut90PhaseOffset() {
+	LocalOscillatorSource lo = LocalOscillatorSource.factory(4, 1, 90);
+
+	Assert.assertEquals(1.0, lo.out(), 0.0001);
+	Assert.assertEquals(0.0, lo.out(), 0.0001);
+	Assert.assertEquals(-1.0, lo.out(), 0.0001);
+	Assert.assertEquals(0.0, lo.out(), 0.0001);
+    }
+
+    @Test
+    public void testOut180PhaseOffset() {
+	LocalOscillatorSource lo = LocalOscillatorSource.factory(4, 1, 180);
+
+	Assert.assertEquals(0.0, lo.out(), 0.0001);
+	Assert.assertEquals(-1.0, lo.out(), 0.0001);
+	Assert.assertEquals(0.0, lo.out(), 0.0001);
+	Assert.assertEquals(1.0, lo.out(), 0.0001);
+    }
+
+    @Test
+    public void testOutNeg90PhaseOffset() {
+	LocalOscillatorSource lo = LocalOscillatorSource.factory(4, 1, -90);
+
+	Assert.assertEquals(-1.0, lo.out(), 0.0001);
+	Assert.assertEquals(0.0, lo.out(), 0.0001);
+	Assert.assertEquals(1.0, lo.out(), 0.0001);
+	Assert.assertEquals(0.0, lo.out(), 0.0001);
+    }
+
 }

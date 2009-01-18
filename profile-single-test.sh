d6ff679eb928d7108487195c81e70dc8551913c6
Karl-Martin Skontorp <kms@skontorp.net>
2009-01-18 20:24:31 +0100

    Added my name and license info.

--- profile-single-test.sh
+++ profile-single-test.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+# sdr101-java
+# Simple software-defined radio for Java.
+# 
+# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://picofarad.org/
+# Licensed under the GNU GPL 2.0 or later.
 
 java -Xprof=help -cp /home/kms/.m2/repository/junit/junit/4.4/junit-4.4.jar:target/classes:target/test-classes org.junit.runner.JUnitCore org.picofarad.sdr101.$1

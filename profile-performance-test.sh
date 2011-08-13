f1ac321e8535b4e6ffd59742ca8f79b714059eed
Karl-Martin Skontorp <kms@skontorp.net>
2011-08-13 14:06:00 +0200

    Update homepage reference.

--- profile-performance-test.sh
+++ profile-performance-test.sh
@@ -2,7 +2,7 @@
 # sdr101-java
 # Simple software-defined radio for Java.
 # 
-# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://picofarad.org/
+# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://22pf.org/
 # Licensed under the GNU GPL 2.0 or later.
 
 time java -Xprof -cp /home/kms/.m2/repository/junit/junit/4.4/junit-4.4.jar:target/classes:target/test-classes org.picofarad.sdr101.Sdr101performance
--- src/test/java/org/picofarad/sdr101/blocks/FilterFactoryTest.java
+++ src/test/java/org/picofarad/sdr101/blocks/FilterFactoryTest.java
@@ -1,7 +1,7 @@
 /* sdr101-java
  * Simple software-defined radio for Java.
  *
- * (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://picofarad.org/
+ * (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://22pf.org/
  * Licensed under the GNU GPL 2.0 or later.
  */
 
